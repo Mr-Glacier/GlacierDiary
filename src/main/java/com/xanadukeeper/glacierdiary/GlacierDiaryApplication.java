c97f0fef976011ac1616f1a6dad0f2a089ce4fe0
Mr-Glacier <K15100415112@163.com>
2024-09-30 14:05:47 +0800

    init

---
+++ src/main/java/com/xanadukeeper/glacierdiary/GlacierDiaryApplication.java
@@ -0,0 +1,17 @@
+package com.xanadukeeper.glacierdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr-Glacier
+ * @since 2024/09/30 8:50
+ */
+@SpringBootApplication
+public class GlacierDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlacierDiaryApplication.class, args);
+    }
+
+}
