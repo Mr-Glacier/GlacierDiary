009f1e9b9cd4797f3149cd279f21c3af18572f95
Mr-Glacier <K15100415112@163.com>
2025-03-23 03:12:17 +0800

    init

---
+++ src/main/java/com/glacier/glacierdiary/GlacierDiaryApplication.java
@@ -0,0 +1,16 @@
+package com.glacier.glacierdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr-glacier
+ */
+@SpringBootApplication
+public class GlacierDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlacierDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glacier/glacierdiary/GlacierDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glacier.glacierdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlacierDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
